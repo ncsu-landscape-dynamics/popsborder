e15912af954cd314340db2dbd06ae561dc58d82a
Anna Petrasova <kratochanna@gmail.com>
2019-01-30 22:07:12 -0500

    adjust script and CI to use arguments

--- test_dataset.sh
+++ test_dataset.sh
@@ -6,6 +6,6 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]
     exit 1
 fi
 
-python simulation.py 1 $3 config.yml \
+python simulation.py --num-simulations 1 --num-shipments $3 --config-file config.yml \
     | grep "F280: " | sed -e 's/F280: //g' \
     | sed -e "s/ /$2/g" > $1
