ddbd359c4ce0254d58a3457f3f72f5958681a871
Vaclav Petras <wenzeslaus@gmail.com>
2020-03-02 16:56:03 -0500

    Add now missing standard output parameter

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 python simulation.py --num-simulations 100 --num-shipments 1000 --config-file config.yml \
     | grep -E '^slippage: [0-9\.]*$' | sed -e 's/.*=//g'
 
-python simulation.py --num-simulations 1 --num-shipments 20 --config-file config.yml \
+python simulation.py --num-simulations 1 --num-shipments 20 --config-file config.yml --output-file "-" \
     | grep "F280: " | sed -e 's/F280: //g'
