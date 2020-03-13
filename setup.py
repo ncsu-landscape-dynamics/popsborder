2d717c5b55cf9673cfc54985dde50417ad4706d1
Vaclav Petras <wenzeslaus@gmail.com>
2020-03-13 15:10:08 -0400

    Add setup.py to install with pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pathways",
+    version="0.0.3",
+    author="Vaclav Petras",
+    author_email="vpetras@ncsu.edu",
+    description="Simulation for evaluation of pathways",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/vpetras/pathway-simulation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Scientific/Engineering",
+    ],
+    python_requires=">=3.6",
+    install_requires=["numpy", "scipy", "PyYAML"],
+)
