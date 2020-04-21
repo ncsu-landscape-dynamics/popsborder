b3371db367edbea1a6fd1752dcf947cc661c3d1e
Vaclav Petras <wenzeslaus@gmail.com>
2020-04-21 11:09:23 -0400

    Use Black

--- pathways/inspections.py
+++ pathways/inspections.py
@@ -265,12 +265,12 @@ def inspect(config, shipment, n_boxes_to_inspect):
     # Inspect selected boxes, count opened boxes, inspected stems, and infested stems
     # to detection and completion
     ret = types.SimpleNamespace(
-    boxes_opened_completion = n_boxes_to_inspect,
-    boxes_opened_detection = 0,
-    stems_inspected_completion = n_boxes_to_inspect * inspect_per_box,
-    stems_inspected_detection = 0,
-    infested_stems_completion = 0,
-    infested_stems_detection = 0,
+        boxes_opened_completion=n_boxes_to_inspect,
+        boxes_opened_detection=0,
+        stems_inspected_completion=n_boxes_to_inspect * inspect_per_box,
+        stems_inspected_detection=0,
+        infested_stems_completion=0,
+        infested_stems_detection=0,
     )
     detected = False
     for i in box_index_to_inspect:
