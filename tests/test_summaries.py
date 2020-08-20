4906586c265b71dba6b953de5f550106891eb8e2
Vaclav Petras <wenzeslaus@gmail.com>
2020-08-19 21:41:51 -0400

    Add export function to pandas and example notebook
    The library is extended only by one function exporting to pandas DataFrame, now mostly (unfortunate) duplicate of CSV export.
    
    A notebook is included which demonstrates the use and adds couple of examples with pandas, plotting, and seaborn.
    
    A simple test is added too just to ensure things are running, but does not perform any checks.

---
+++ tests/test_summaries.py
@@ -0,0 +1,31 @@
+from pathways.scenarios import run_scenarios, load_scenario_table
+from pathways.simulation import load_configuration
+from pathways.outputs import save_scenario_result_to_pandas
+
+
+def test_scenarios(datadir, tmp_path):
+    basic_config = load_configuration(datadir / "config.yml")
+    scenario_table = load_scenario_table(datadir / "scenarios_config.csv")
+    results = run_scenarios(
+        config=basic_config,
+        scenario_table=scenario_table,
+        seed=42,
+        num_simulations=2,
+        num_shipments=10,
+    )
+    assert len(scenario_table) == len(results)
+    df = save_scenario_result_to_pandas(
+        results,
+        config_columns=[
+            "name",
+            "shipment/boxes/min",
+            "shipment/boxes/max",
+            "shipment/stems_per_box/default",
+            "pest/infestation_rate/parameters",
+        ],
+        result_columns=[
+            "missing",
+            "avg_boxes_opened_completion",
+            "avg_boxes_opened_detection",
+        ],
+    )
