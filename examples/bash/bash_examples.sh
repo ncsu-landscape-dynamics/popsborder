d6f6e243c3ff76aa61deff29d4d80d9193237aed
Vaclav Petras <wenzeslaus@gmail.com>
2020-12-22 14:04:34 -0500

    Add Bash scripts to CI, organize as examples, restore code compatibility (#102)
    Replaces a command in CI by the scripts we provide in the repo.
    
    Bash script are now in a separate examples directory with their own config file.
    
    Fixes Consignment class so that it works with outputs which still use the dict syntax.

---
+++ examples/bash/bash_examples.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This file contains examples of using the simulation in Bash.
+# To certain extent it will be applicable to other command lines (shells).
+# This script can run as is in its directory in the repo.
+
+python -m pathways \
+    --num-simulations 10 --num-consignments 100 --config-file config.yml \
+    | grep -E '^slippage: [0-9\.]*$' | sed -e 's/.*=//g'
+
+python -m pathways \
+    --num-simulations 1 --num-consignments 20 --config-file config.yml --output-file - \
+    | grep "F280: " | sed -e 's/F280: //g'
