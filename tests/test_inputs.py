726f98bcef9d4a3768d6fdbb95416f9a4569d4ad
Vaclav Petras <wenzeslaus@gmail.com>
2022-02-16 15:57:10 -0500

    Fix and improve the boolean tests

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -9,20 +9,20 @@ from popsborder.inputs import (
 
 def test_text_to_value_boolean_json_and_canonical_yaml():
     """Check that JSON representation of boolean values yields booleans"""
-    assert text_to_value("true") == True
-    assert text_to_value("false") == False
+    assert text_to_value("true") is True
+    assert text_to_value("false") is False
 
 
 def test_text_to_value_boolean_python_and_extra_yaml():
     """Check that Python-like, non-canonical YAML representation yields booleans"""
-    assert text_to_value("True") == True
-    assert text_to_value("False") == False
+    assert text_to_value("True") is True
+    assert text_to_value("False") is False
 
 
 def test_text_to_value_boolean_all_caps_extra_yaml():
     """Check that all caps, non-canonical YAML representation yields booleans"""
-    assert text_to_value("TRUE") == True
-    assert text_to_value("FALSE") == False
+    assert text_to_value("TRUE") is True
+    assert text_to_value("FALSE") is False
 
 
 @pytest.mark.parametrize(
@@ -63,7 +63,11 @@ def test_text_to_value_boolean_like_text_still_text(text):
 def test_text_to_value_boolean_like_value_still_value(value, expected):
     """Check that non-text boolean-like values have proper value and type"""
     new_value = text_to_value(value)
+    print(new_value, type(new_value))
     assert new_value == expected
     # Integers and floats 0 and 1 actually compare True against booleans,
     # so we need to check the type as well.
-    assert type(new_value) == type(expected)
+    assert isinstance(new_value, type(expected))
+    # We are allow for inheritance in the type check, however
+    # the class bool inherits from int, so we need to check that it is not bool.
+    assert not isinstance(new_value, bool)
