3469300c995c4e2471da31d7087f468976cee7fd
Vaclav Petras <wenzeslaus@gmail.com>
2022-02-16 16:08:15 -0500

    Check for all booleans and fix pylint

--- popsborder/inputs.py
+++ popsborder/inputs.py
@@ -36,6 +36,8 @@ def text_to_value(arg):
     If conversion is not possible, the original text is returned.
     None is returned for both None and an empty string.
     """
+    # This is a short function with return statements only.
+    # pylint: disable=too-many-return-statements
     if not isinstance(arg, str):
         return arg
     if not arg:
@@ -46,11 +48,11 @@ def text_to_value(arg):
         try:
             return float(arg)
         except ValueError:
-            # Support Python-like booleans and all YAML booleans.
+            # Supports JSON booleans, Python-like booleans, and all YAML booleans.
             # We do not support old YAML booleans.
-            if arg == "True" or arg == "TRUE":
+            if arg in ["true", "True", "TRUE"]:
                 return True
-            if arg == "False" or arg == "FALSE":
+            if arg in ["false", "False", "FALSE"]:
                 return False
             try:
                 return json.loads(arg)
