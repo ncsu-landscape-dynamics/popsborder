c7362902d24d414183352c0b5e4c2f6b5f4fd7bb
Vaclav Petras <wenzeslaus@gmail.com>
2022-03-18 13:51:08 -0400

    Convert to pandas from one result with all columns (#169)
    * Add conversion from one result to pandas data frame.
    * By default, convert all config items and all result attributes to columns and values in a new data frame.
    * Allows providing no config for converting one result.

--- tests/test_summaries.py
+++ tests/test_summaries.py
@@ -1,9 +1,90 @@
+"""Test functions which output to pandas"""
+
+import pytest
+
 from popsborder.inputs import load_configuration, load_scenario_table
-from popsborder.outputs import save_scenario_result_to_pandas
-from popsborder.scenarios import run_scenarios
+from popsborder.outputs import (
+    save_scenario_result_to_pandas,
+    save_simulation_result_to_pandas,
+)
+from popsborder.scenarios import run_scenarios, run_simulation
 
 
-def test_scenarios(datadir, tmp_path):
+NUM_ITEMS_IN_CONFIG = 33
+NUM_ATTRIBUTES_IN_RESULT = 25
+
+
+@pytest.fixture(name="config")
+def fixture_config(datadir):
+    """Get loaded configuration"""
+    return load_configuration(datadir / "config.yml")
+
+
+@pytest.fixture(name="result")
+def fixture_executed_simulation_result(config):
+    """Get result of a simulation after running it"""
+    result = run_simulation(
+        config=config, seed=42, num_simulations=2, num_consignments=10
+    )
+    return result
+
+
+def test_simulation_to_pandas_default_columns(result, config):
+    """Check that one result with config coverts with default column values"""
+    df = save_simulation_result_to_pandas(result, config)
+    print(df)
+    shape = df.shape
+    assert len(shape) == 2
+    rows, cols = shape
+    assert cols == NUM_ITEMS_IN_CONFIG + NUM_ATTRIBUTES_IN_RESULT
+    assert rows == 1
+
+
+def test_simulation_to_pandas_no_config(result):
+    """Check that one result without config coverts"""
+    df = save_simulation_result_to_pandas(result)
+    shape = df.shape
+    assert len(shape) == 2
+    rows, cols = shape
+    assert cols == NUM_ATTRIBUTES_IN_RESULT
+    assert rows == 1
+
+
+def test_simulation_to_pandas_no_config_columns(result, config):
+    """Check that result coverts with no config column if columns is an empty list"""
+    df = save_simulation_result_to_pandas(result, config, config_columns=[])
+    shape = df.shape
+    assert len(shape) == 2
+    rows, cols = shape
+    assert cols == NUM_ATTRIBUTES_IN_RESULT
+    assert rows == 1
+
+
+def test_simulation(result, config):
+    """Result converts to data frame with specified columns"""
+    config_columns = [
+        "consignment/parameter_based/boxes/min",
+        "consignment/parameter_based/boxes/max",
+        "consignment/items_per_box/default",
+        "contamination/contamination_rate/parameters",
+    ]
+    result_columns = [
+        "missing",
+        "avg_boxes_opened_completion",
+        "avg_boxes_opened_detection",
+    ]
+    df = save_simulation_result_to_pandas(
+        result, config, config_columns=config_columns, result_columns=result_columns
+    )
+    shape = df.shape
+    assert len(shape) == 2
+    rows, cols = shape
+    assert cols == len(config_columns) + len(result_columns)
+    assert rows == 1
+
+
+def test_scenarios(datadir):
+    """Scenario results convert to data frame with specified columns"""
     basic_config = load_configuration(datadir / "config.yml")
     scenario_table = load_scenario_table(datadir / "scenarios_config.csv")
     results = run_scenarios(
